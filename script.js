3f99b500d7ae952a150d144936e6e8005c45f79e
Muhmmad Sameer <muhmmadsameer143@gmail.com>
2023-05-13 16:20:57 +0530

    first commit

--- script.js
+++ script.js
@@ -8,20 +8,43 @@ let arr = [
 
 function PrintStudentswithMap() {
   //Write your code here , just console.log
+  arr.map(function (value) {
+    if (value.marks > 50) {
+      console.log(value);
+    }
+  });
+
 }
 
 function PrintStudentsbyForEach() {
   //Write your code here , just console.log
+  arr.forEach(function (value) {
+    if (value.marks > 50) {
+      console.log(value);
+    }
+  });
+
 }
 
 function addData() {
   //Write your code here, just console.log
+  arr.push({ id:4,name:"new Added Student",age:"20",marks:45 });
+  console.log(arr);
+
 }
 
 function removeFailedStudent() {
   //Write your code here, just console.log
+  let filterArr = arr.filter((arr) => arr.marks < 50);
+  console.log(filterArr);
+
+
 }
 
 function concatenateArray() {
   //Write your code here, just console.log
+  let newArr = [{ id: 8, name: "New Student", age: "23",marks:90 }];
+  let concatArr = arr.concat(newArr);
+  console.log(concatArr);
+
 }
